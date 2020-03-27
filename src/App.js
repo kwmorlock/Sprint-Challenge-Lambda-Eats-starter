6e7365fa0b554a1fc18509e99463f011010da8ba
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-03-27 11:15:34 -0400

    yup and axios installed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <p>Free flex with every order</p>
     </>
   );
 };
